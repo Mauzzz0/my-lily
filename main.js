ffd9c55ead1a8ab7373d474adee507e3568fd43f
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-12-16 18:30:52 +0300

    Проинициализирован NPM проект

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-console.log(777);
-console.log(777);
-console.log(222);
-console.log(777);
-console.log(333);
-console.log(777);
-console.log(666);
-console.log(777);
+const       askdajsdn     =             10
